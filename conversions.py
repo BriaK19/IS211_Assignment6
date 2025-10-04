1e11eb42cabdd6739e652f94befeaf56b62cd69c
BriaK19 <bria.king61@spsmail.cuny.edu>
2025-10-04 13:04:53 -0400

    Update conversions.py

--- conversions.py
+++ conversions.py
@@ -3,3 +3,15 @@ def convertCelsiusToKelvin(celsius: float) -> float:
 
 def convertCelsiusToFahrenheit(celsius: float) -> float:
   return (celsius * 9/5) + 32
+
+def convertFahrenheitToCelsius(farenheit: float) -> float:
+  return (fahrenheit - 32) * 5/9
+
+def convertFahrenheitToKelvin(fahrenheit: float) -> float:
+  return (fahrenheit - 32) * 5/9 + 273.15
+
+def convertKelvinToCelsius(kelvin: float) -> float:
+  return kelvin - 273.15
+
+def convertKelvinToFahrenheit(kelvin: float) -> float:
+  return (kelvin - 273.15) * 9/5 + 32
